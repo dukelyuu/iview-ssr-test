2ed9eb2b69cc1f5d594c2d243016679fccdfd32c
Lawrence Lee <lawrence.lee@foxmail.com>
2017-07-10 17:59:03 +0800

    add TestCase component to wrap test case

---
+++ components/TestCase.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test-case">
+    <div class="test-case-title">
+      {{ title }}
+    </div>
+    <div class="test-case-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['title']
+}
+</script>
+<style lang="less">
+.test-case {
+  &-title {
+
+  }
+}
+</style>
